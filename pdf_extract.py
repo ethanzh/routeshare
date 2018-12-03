26c1d145308a33f90e15dd9ebd99168956646387
Ethan Houston <ethan.houston@gmail.com>
2018-12-02 20:25:35 -0600

    Create course data CSV

--- pdf_extract.py
+++ pdf_extract.py
@@ -1,7 +1,15 @@
+import pandas as pd
 import textract
-text = textract.process('hi.pdf', encoding='ascii')
-text = text.decode("utf-8") 
+text = textract.process('course.pdf', encoding='ascii')
+text = text.decode("utf-8")
 lines = text.split("\n")
+
+df_data = {
+    "code": [],
+    "days": [],
+    "times": [],
+    "buildings": []
+}
 
 for i in range(len(lines)):
     current_line = lines[i]
@@ -16,7 +24,12 @@ for i in range(len(lines)):
             # times are index 0, 2, building is 3
             times = (data[0], data[2])
             building = data[3]
-            print(code, day, times, building)
+            df_data["code"].append(code)
+            df_data["days"].append(day)
+            df_data["times"].append(times)
+            df_data["buildings"].append(building)
+
+            #print(code, day, times, building)
         # the line after is the professor's name, so go ahead by 2 more (3 total)
         next_line = lines[i + 3]
         if (len(next_line) == 1):
@@ -27,6 +40,13 @@ for i in range(len(lines)):
                 if len(data) > 4:
                     times = (data[0], data[2])
                     building = data[3]
-                    print(" " * 6, day, times, building)
+                    #print(" " * 6, day, times, building)
+                    df_data["code"].append(code)
+                    df_data["days"].append(day)
+                    df_data["times"].append(times)
+                    df_data["buildings"].append(building)
             except IndexError:
                 print(data)
+
+df = pd.DataFrame(data=df_data)
+df.to_csv("course_data.csv", encoding='utf-8')
